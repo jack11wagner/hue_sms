ac85288634793e1c2d5b7dc2610cfb287099508c
jack11wagner <wagnerj05@moravian.edu>
2021-07-09 11:10:36 -0400

    updated file based on Seth's comments from pull request

--- src/plotlydash.py
+++ src/plotlydash.py
@@ -9,37 +9,37 @@ external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
-file = 'colorCount.csv'
 r = redis.Redis(
     host='localhost', port=6379, db=0)
-def setup():
-    # getting colors totals into a dict from redis
-    color_byte_dict = r.hgetall('color_totals')
-    color_totals_dict = {}
+def decode_redis(filename):
+    color_byte_dict = r.hgetall(filename)
+    color_dict = {}
     for key in color_byte_dict:
         value = color_byte_dict[key].decode('utf-8')
-        key = key.decode('utf-8')
-        color_totals_dict[key] = int(value)
+        key = key.decode('utf-8').title()
+        color_dict[key] = value
+    return color_dict
+
+def setup():
+    # getting colors totals into a dict from redis
+    color_totals_dict = decode_redis('color_totals')
+    for color in color_totals_dict:
+        color_totals_dict[color] = int(color_totals_dict[color])
 
     # getting colors RGB values into a dict from redis
-    rediscolor_RGB_dict = r.hgetall('colors')
-    colorsRGBdict = {}
-    for key in rediscolor_RGB_dict:
-        value = rediscolor_RGB_dict[key].decode('utf-8')
-        red, green, blue = value.split(',')
-        key = key.decode('utf-8')
-        colorsRGBdict[key.title()] = px.colors.label_rgb((int(red), int(green), int(blue)))
+    color_RGB_dict = decode_redis('colors')
+    for color in color_RGB_dict:
+        red, green, blue = color_RGB_dict[color].split(',')
+        color_RGB_dict[color.title()] = px.colors.label_rgb((int(red), int(green), int(blue)))
 
-    labels = []
-    sizes = []
-    colors = []
+    labels, sizes, colors = [], [], []
 
     for key in color_totals_dict:
         if (color_totals_dict[key] > 0):
             labels.append(key.title())
             sizes.append(color_totals_dict[key])
             colors.append(key.title())
-    fig = px.pie(names=labels, values=sizes, color=labels, color_discrete_map=colorsRGBdict, width=1200, height=600)
+    fig = px.pie(names=labels, values=sizes, color=labels, color_discrete_map=color_RGB_dict, width=1200, height=600)
 
     app.layout = html.Div(children=[
         html.H1(children='Moravian Color Choices'),
@@ -70,32 +70,25 @@ def setup():
 @app.callback(Output('colors-graph', 'figure'),
               Input('interval-component', 'n_intervals'))
 def update_graph_live(n):
-    color_byte_dict = r.hgetall('color_totals')
-    color_totals_dict = {}
-    for key in color_byte_dict:
-        value = color_byte_dict[key].decode('utf-8')
-        key = key.decode('utf-8')
-        color_totals_dict[key] = int(value)
+    # getting colors totals into a dict from redis
+    color_totals_dict = decode_redis('color_totals')
+    for color in color_totals_dict:
+        color_totals_dict[color] = int(color_totals_dict[color])
 
     # getting colors RGB values into a dict from redis
-    rediscolor_RGB_dict = r.hgetall('colors')
-    colorsRGBdict = {}
-    for key in rediscolor_RGB_dict:
-        value = rediscolor_RGB_dict[key].decode('utf-8')
-        red, green, blue = value.split(',')
-        key = key.decode('utf-8')
-        colorsRGBdict[key.title()] = px.colors.label_rgb((int(red), int(green), int(blue)))
+    color_RGB_dict = decode_redis('colors')
+    for color in color_RGB_dict:
+        red, green, blue = color_RGB_dict[color].split(',')
+        color_RGB_dict[color.title()] = px.colors.label_rgb((int(red), int(green), int(blue)))
 
-    labels = []
-    sizes = []
-    colors = []
+    labels, sizes, colors = [], [], []
 
     for key in color_totals_dict:
         if (color_totals_dict[key] > 0):
             labels.append(key.title())
             sizes.append(color_totals_dict[key])
             colors.append(key.title())
-    fig = px.pie(names=labels, values=sizes, color=labels, color_discrete_map=colorsRGBdict)
+    fig = px.pie(names=labels, values=sizes, color=labels, color_discrete_map=color_RGB_dict)
 
     return fig
 
