b11bc6aa02fdc4ec0fdfa40bc962beace248bcfb
jack11wagner <jacksonjw519@gmail.com>
2021-12-14 18:36:03 -0500

    Changed FuzzyColors to get colors list from Redis rather than csv
    * This fixes the error we were having with not being able to find the colors.csv file
    * This way we can just query redis for the colors list for fuzzy string matching

--- src/fuzzyColors.py
+++ src/fuzzyColors.py
@@ -1,12 +1,16 @@
 from thefuzz import fuzz, process
-import pandas as pd
+import redis
 
-colors_list = pd.read_csv('colors.csv', names=['Color', 'R', 'G', 'B'])
+redis = redis.Redis(host='localhost', port=6379, db=0)
+colors_list = []
+for color in redis.hgetall('color_totals').keys():
+    color = color.decode('utf-8')
+    colors_list.append(color)
 
 
 def getFuzzyColor(color_name):
     # we want a 85% match for thefuzz
-    fuzzyMatch = process.extractOne(color_name.title(), colors_list['Color'], scorer=fuzz.token_sort_ratio)
+    fuzzyMatch = process.extractOne(color_name.title(), colors_list, scorer=fuzz.token_sort_ratio)
     fuzz_color, percent_match = fuzzyMatch[0], fuzzyMatch[1]
     if percent_match >= 85:
         return fuzz_color
